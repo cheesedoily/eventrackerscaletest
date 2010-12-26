a09b2fc3144f660c02e7a794fe0b1d897d71761f
Nafis Jamal <njamal@Macintosh.local>
2010-12-26 15:31:29 -0800

    added time to counter

--- counters/models.py
+++ counters/models.py
@@ -3,6 +3,7 @@ from google.appengine.ext import db
 class Counter(db.Model):
     dimension_one = db.ReferenceProperty(collection_name="dim_one_counters") # generic reference
     dimension_two = db.ReferenceProperty(collection_name="dim_two_counters") # generic reference
+    date_hour = db.DateTimeProperty()
     count_one = db.IntegerProperty() 
     count_two = db.IntegerProperty()
     count_three = db.IntegerProperty()
